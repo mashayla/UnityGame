802374454bf45a66f785863e59070106560583c2
JayHong95 <hjh562@gmail.com>
2024-03-26 13:28:17 -0400

    Fixed some errors

--- CSGame/Assets/Scripts/ActionController.cs
+++ CSGame/Assets/Scripts/ActionController.cs
@@ -1,69 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class ActionController : MonoBehaviour
 {
-
     [SerializeField]
-    private float range;
-    
+    private float range = 20f;
     private bool pickupActivated = false;
-
-
     private RaycastHit hitInfo;
-
     [SerializeField]
     private LayerMask layerMask;
- 
     [SerializeField]
     private Text actionText;
-
     [SerializeField]
     private Inventory theInventory;
 
-    // Update is called once per frame
     void Update()
     {
         CheckItem();
         TryAction();
-
     }
-    private void TryAction(){
-        if (Input.GetKeyDown(KeyCode.Z)){
+
+    private void TryAction()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            range += 5F;
             CheckItem();
             CanPickUp();
         }
     }
 
-    private void CanPickUp(){
-        if (pickupActivated){
-            if(hitInfo.transform != null){
-                Debug.Log("I got " + hitInfo.transform.GetComponent<ItemPickUp>().item.itemName);
-                theInventory.AcquireItem(hitInfo.transform.GetComponent<ItemPickUp>().item);
-                Destroy(hitInfo.transform.gameObject);
-                InfoDisappear();
-            }
+    private void CanPickUp()
+    {
+        if (pickupActivated && hitInfo.transform != null)
+        {
+            Debug.Log("I got " + hitInfo.transform.GetComponent<ItemPickUp>().item.itemName);
+            theInventory.AcquireItem(hitInfo.transform.GetComponent<ItemPickUp>().item);
+            Destroy(hitInfo.transform.gameObject);
+            InfoDisappear();
         }
     }
 
-    private void CheckItem(){
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitInfo, range,layerMask)){
-            if(hitInfo.transform.tag == "Item"){
+    private void CheckItem()
+    {
+        // Corrected raycast direction
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, range, layerMask))
+        {
+            if (hitInfo.transform.tag == "Item")
+            {
                 ItemInfoAppear();
             }
         }
         else
+        {
             InfoDisappear();
+        }
     }
 
-    private void ItemInfoAppear(){
+    private void ItemInfoAppear()
+    {
         pickupActivated = true;
         actionText.gameObject.SetActive(true);
         actionText.text = hitInfo.transform.GetComponent<ItemPickUp>().item.itemName;
     }
 
-    private void InfoDisappear(){
+    private void InfoDisappear()
+    {
         pickupActivated = false;
         actionText.gameObject.SetActive(false);
     }
--- CSGame/Assets/Scripts/Inventory.cs
+++ CSGame/Assets/Scripts/Inventory.cs
@@ -1,52 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using JetBrains.Annotations;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
     public static bool inventoryActivated = false;
-    
     [SerializeField]
     private GameObject go_InventoryBase;
     [SerializeField]
     private GameObject go_SlotsParent;
-
     private Slots[] slots;
 
-
-    // Start is called before the first frame update
     void Start()
-    {   
+    {
         slots = go_SlotsParent.GetComponentsInChildren<Slots>();
-
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
         TryOpenInventory();
     }
 
     void TryOpenInventory()
-{
-    if(Input.GetKeyDown(KeyCode.I))
     {
-        inventoryActivated = ! inventoryActivated;
-
-        if(inventoryActivated)
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            OpenInventory();
-        }
-        else
-        {
-            CloseInventory();
+            inventoryActivated = !inventoryActivated;
+            if (inventoryActivated)
+            {
+                OpenInventory();
+            }
+            else
+            {
+                CloseInventory();
+            }
         }
     }
-}
 
     private void OpenInventory()
     {
@@ -58,26 +45,39 @@ public class Inventory : MonoBehaviour
         go_InventoryBase.SetActive(false);
     }
 
-public void AcquireItem(Item _item, int _count = 1){
+    public void AcquireItem(Item _item, int _count = 1)
+    {
+        bool itemAdded = false;
 
-    if(Item.ItemType.Equipment != _item.itemType){
-        for (int i = 0; i < slots.Length; i++){
-            if(slots[i].item != null){
-                if(slots[i].item.itemName == _item.itemName){
+        // First, try to add to an existing slot with the same item
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item != null && slots[i].item.itemName == _item.itemName)
+            {
                 slots[i].SetSlotCount(_count);
-                return;
-            }
+                itemAdded = true;
+                break;
             }
         }
 
-    }
-    for (int i = 0; i < slots.Length; i++){
-        if(slots[i].item.itemName == null){
-            slots[i].Additem(_item, _count);
-            return;
+        // If the item wasn't added to an existing slot, find an empty slot
+        if (!itemAdded)
+        {
+            for (int i = 0; i < slots.Length; i++)
+            {
+                if (slots[i].item == null)
+                {
+                    slots[i].Additem(_item, _count);
+                    itemAdded = true;
+                    break;
+                }
+            }
         }
 
+        // Optionally, handle the case where no slots are available
+        if (!itemAdded)
+        {
+            Debug.Log("No available slots for " + _item.itemName);
+        }
     }
-}
-
 }
