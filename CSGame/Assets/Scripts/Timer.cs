755957620b28f3af876018c994df351e3acaee49
Mashayla Banks <121362585+mashayla@users.noreply.github.com>
2024-04-09 17:49:32 -0400

    added timer script and UI

---
+++ CSGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; // Change this line to import TMPro namespace
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 60;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI timeText;
+
+    // starts timer
+    void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update() // timer begins countdown
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out...You Died");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay +=1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes,seconds);
+    }
+}
